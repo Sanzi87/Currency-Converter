8e67832d34d3c8a208273322730b571757861845
Alexander Blomberg <info@dalasoftware.se>
2025-02-09 16:13:52 +0100

    Make a plan

---
+++ currency_converter.py
@@ -0,0 +1,14 @@
+# Loop
+# Ask the user for an amoint
+# If invaild -> Print an error
+
+# Loop
+# Ask for the source currency
+# If invaild -> Print an error
+
+# Loop
+# Ask for the target currency
+# #If invaild -> Print an error
+
+# Do the conversion
+# Print the result
